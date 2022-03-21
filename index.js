4255ee523fc805f0413355ef5d11d428b1d9b3c8
Will <will@containerlabs.io>
2022-03-21 11:29:40 -0700

    add start command

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const port = parseInt(process.env.PORT) || 5000;
+const port = parseInt(process.env.PORT) || 5005;
 const USERS = [
     {
         id: 1,
